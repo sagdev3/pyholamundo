1aae12c38aa3242eb35eba902376dc6642788076
sagsilver <sagdevweb@gmail.com>
2023-12-28 22:49:19 -0500

    SIGUIENDO LISTAS

--- tipos-avanzados/02-manipulando-listas.py
+++ tipos-avanzados/02-manipulando-listas.py
@@ -1,2 +1,12 @@
 mascotas = ["Wolfgang", "Pelusa", "Pulga", "Copito"]
 print(mascotas[0])
+
+mascotas[0] = 'Bicho'
+print(mascotas)
+
+print(mascotas[2:])
+print(mascotas[-1])
+print(mascotas[1::2])
+
+numeros = list(range(21))
+print(numeros[1::2])
